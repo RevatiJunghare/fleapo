df6c9174f3a96cbe9dcc3d34320fc8808e2988a8
revati junghare <revati.junghare@geta.ai>
2024-07-17 18:01:14 +0530

    css_changes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function myFunction() {
     }
   }
 
+  
+
 
   document.querySelectorAll('.slider').forEach(slider => {
     let currentIndex = 0;
@@ -15,8 +17,6 @@ function myFunction() {
     const slideCount = slides.children.length;
     const visibleSlides = 3;
     const maxIndex = slideCount - visibleSlides;
-    const prevButton = slider.querySelector('.prev');
-    const nextButton = slider.querySelector('.next');
 
     function moveSlides(direction) {
         currentIndex += direction;
@@ -24,15 +24,18 @@ function myFunction() {
         if (currentIndex < 0) {
             currentIndex = 0;
         } else if (currentIndex > maxIndex) {
-            currentIndex = maxIndex;
+            currentIndex = 0;
         }
 
         const slideWidth = slides.children[0].offsetWidth;
         slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
     }
 
-    prevButton.addEventListener('click', () => moveSlides(-1));
-    nextButton.addEventListener('click', () => moveSlides(1));
+    function autoSlide() {
+      moveSlides(1); // Automatically move slides forward
+  }
+
+  setInterval(autoSlide, 2000);
 });
 
 
@@ -42,49 +45,9 @@ window.addEventListener('resize', () => {
 });
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const sliders = document.querySelectorAll('.slider');
-  
-//   sliders.forEach(slider => {
-//       const slides = slider.querySelectorAll('.slide');
-//       const totalSlides = slides.length;
-//       console.log(totalSlides)
-//       let slideIndex = 0;
-      
-//       const showSlides = (index) => {
-//           if (index > totalSlides) {
-//               slideIndex = 0;
-//           } else if (index < 0) {
-//               slideIndex = totalSlides - 1;
-//           } else {
-//               slideIndex = index;
-//           }
-//           const offset = -slideIndex * 100;
-//           slider.querySelector('.slides').style.transform = `translateX(${offset}%)`;
-//       };
-
-//       const moveSlides = (n) => {
-//           showSlides(slideIndex + n);
-//       };
-
-//       slider.querySelector('.prev').addEventListener('click', () => {
-//           moveSlides(-1);
-//       });
-
-//       slider.querySelector('.next').addEventListener('click', () => {
-//           moveSlides(1);
-//       });
-
-      
-//       showSlides(slideIndex);
-//   });
-// });
 
 
-// window.addEventListener('resize', () => {
-//   document.querySelector('.slides').style.transform = 'translateX(0px)'; 
-//   currentIndex = 0; 
-// });
+
 
 
 
